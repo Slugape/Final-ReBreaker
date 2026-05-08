3aec5abb63340f3177b114fda5fd025daf4d164e
Slugape <luiscue299@gmail.com>
2026-05-07 23:23:40 -0400

    Created Start Scene

---
+++ Assets/Scripts/Start Screen Script/Start Button.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StartButton : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
